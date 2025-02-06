a32f1ff7b8610976fc939a671a4c0658f8d925f4
sli012 <sl5406021@gmail.com>
2025-02-06 22:18:45 +0000

    Added border and has full width on the page and centered

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <CourseGraph />
+      <div className="flex flex-col items-center justify-center min-h-screen p-10"> 
+        <div className="w-full max-w-4xl border-2 border-gray-400 rounded-lg p-10"> 
+          <CourseGraph />
+         </div>   
+       </div>
     </div>
+
   );
 }
 
